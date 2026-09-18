21bc68471e3231cfc81dd07be17d77218f5d3594
Mrugakshi Nilesh Shete <shetemrugakshi4@gmail.com>
2026-09-19 00:33:31 +0530

    connecting backend

--- greencampus/backend/main.py
+++ greencampus/backend/main.py
@@ -42,7 +42,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://greencampus-ai-2.onrender.com",
+        "https://greencampus-ai-1.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
